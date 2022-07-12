860f864dfb183ef4239a1244ae6041a53403712d
Abdulsalamsawalha <abdulsalamsawalha032@gmail.com>
2022-07-12 16:47:35 +0300

    update

---
+++ app/src/main/java/com/ssgosoft/astv/Model/ChannelModel.java
@@ -0,0 +1,13 @@
+package com.ssgosoft.astv.Model;
+
+public class ChannelModel {
+    private  String name;
+    public  ChannelModel(String name)
+    {
+        this.name = name;
+    }
+    public String getNameChannel()
+    {
+        return name;
+    }
+}
--- app/src/main/java/com/ssgosoft/astv/ViewModel/ChannelViewModel.java
+++ app/src/main/java/com/ssgosoft/astv/ViewModel/ChannelViewModel.java
@@ -1,19 +1,32 @@
 package com.ssgosoft.astv.ViewModel;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.ssgosoft.astv.Model.ChannelModel;
 import com.ssgosoft.astv.R;
+
+import java.util.ArrayList;
 
 public class ChannelViewModel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activite_channels);
+        setContentView(R.layout.activity_channel);
+        ListView listView = findViewById(R.id.list_channel);
+        ArrayList<ChannelModel> item = new ArrayList<ChannelModel>();
+        item.add(new ChannelModel("الاعراف"));
+        item.add(new ChannelModel(" الباسط"));
+
+        AdapterViewModel channelAdpaterViewModel = new AdapterViewModel(getApplicationContext(),item);
+        listView.setAdapter(channelAdpaterViewModel);
+
+        }
+
+
+
     }
 
-
-
-}
